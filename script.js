81c332655b01628066317fa4d29cc0a2b209ac8a
Dannyestrella090 <dest8152@giant.cos.edu>
2025-03-24 21:36:08 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    const message = "LeBron inspires us with his greatness, leadership, and dedication both on and off the court!";
+    document.getElementById("fanMessage").innerText = message;
+}
